5a6ac96d14529ceafa2f38827acbafe2dd621cdd
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-13 18:41:24 -0300

    Ajustando onEventDragFinish do Kalend

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import { useState } from 'react';
 import style from './App.module.scss';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
-import { IEvento } from './interfaces/IEvento';
 import Calendario from './components/Calendario';
 import ListaDeEventos from './components/ListaDeEventos';
 import { RecoilRoot } from 'recoil'
 
 function App() {
 
-  const [filtro, setFiltro] = useState<Date | null>()
-
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round((new Date()).getTime() / 1000)
-
-  }
-
   const aplicarFiltro = (data: Date | null) => {
-    setFiltro(data)
+    // setFiltro(data)
   }
 
   return (
